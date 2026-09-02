3f7d836621f46a97dbd0932dac5d124f6d187304
Renato Golia <renato@golia.tech>
2026-09-03 01:27:06 +0200

    Add EventBridge event adapter

---
+++ packages/aws/adapters/eventbridge.ts
@@ -0,0 +1,34 @@
+import type { EventDocument } from "@hooksmith/core";
+
+export interface EventBridgeEvent<TDetail = unknown> {
+  version: string;
+  id: string;
+  "detail-type": string;
+  source: string;
+  account: string;
+  time: string;
+  region: string;
+  resources: string[];
+  detail: TDetail;
+}
+
+export function fromEventBridge<TData = unknown>(
+  event: EventBridgeEvent<TData>,
+): EventDocument<TData> {
+  return {
+    type: event["detail-type"],
+    timestamp: Temporal.Instant.from(event.time).toString(),
+    source: {
+      kind: "aws.eventbridge",
+      id: event.source,
+    },
+    metadata: {
+      id: event.id,
+      version: event.version,
+      account: event.account,
+      region: event.region,
+      resources: event.resources,
+    },
+    data: event.detail,
+  };
+}
