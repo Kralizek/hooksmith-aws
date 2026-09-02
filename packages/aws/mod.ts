09bd1aacb7e0ac5bae393dfdd4386664b63e853f
Renato Golia <renato@golia.tech>
2026-09-03 01:28:22 +0200

    Export AWS adapters and listeners

--- packages/aws/mod.ts
+++ packages/aws/mod.ts
@@ -4,4 +4,11 @@
  * @module
  */
 
-export {};
+export * from "./adapters/eventbridge.ts";
+export * from "./adapters/sns.ts";
+export * from "./adapters/sqs.ts";
+export * from "./listeners/eventbridge.ts";
+export * from "./listeners/lambda.ts";
+export * from "./listeners/sns.ts";
+export * from "./listeners/sqs.ts";
+export type { ValueOrFactory } from "./shared/value.ts";
