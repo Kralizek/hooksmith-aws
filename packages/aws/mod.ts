3f3e80f656ed658bd31910c5171032faaffd455f
Renato Golia <renato@golia.tech>
2026-09-03 02:05:11 +0200

    Add service-specific AWS exports

--- packages/aws/mod.ts
+++ packages/aws/mod.ts
@@ -1,14 +1,10 @@
 /**
- * AWS event adapters and service listeners for Hooksmith.
+ * Shared AWS integration types for Hooksmith.
+ *
+ * Import service integrations from the corresponding subpath, such as
+ * `@hooksmith/aws/sqs` or `@hooksmith/aws/eventbridge`.
  *
  * @module
  */
 
-export * from "./adapters/eventbridge.ts";
-export * from "./adapters/sns.ts";
-export * from "./adapters/sqs.ts";
-export * from "./listeners/eventbridge.ts";
-export * from "./listeners/lambda.ts";
-export * from "./listeners/sns.ts";
-export * from "./listeners/sqs.ts";
 export type { ValueOrFactory } from "./shared/value.ts";
---
+++ packages/aws/sqs.ts
@@ -0,0 +1,8 @@
+/**
+ * Amazon SQS integrations for Hooksmith.
+ *
+ * @module
+ */
+
+export * from "./adapters/sqs.ts";
+export * from "./listeners/sqs.ts";
