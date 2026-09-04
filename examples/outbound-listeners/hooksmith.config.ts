876273ba8137d29217e60ac204391afe5f03c5ac
Renato Golia <renato@golia.tech>
2026-09-04 14:25:25 +0200

    Show AWS enrichers in outbound example

--- examples/outbound-listeners/hooksmith.config.ts
+++ examples/outbound-listeners/hooksmith.config.ts
@@ -1,8 +1,22 @@
 import type { Config } from "@hooksmith/core";
 import { putEventBridgeEvent } from "@hooksmith/aws/eventbridge";
+import { getParameterEnrichment } from "@hooksmith/aws/ssm";
 import { sendSqsMessage } from "@hooksmith/aws/sqs";
+import { getCallerIdentityEnrichment } from "@hooksmith/aws/sts";
 
 export default {
+  enrichers: [
+    getCallerIdentityEnrichment(),
+    getParameterEnrichment({
+      parameterName: Deno.env.get("TENANT_PLAN_PARAMETER")!,
+      withDecryption: true,
+      map: (_event, response) => ({
+        metadata: {
+          tenantPlan: response.Parameter?.Value,
+        },
+      }),
+    }),
+  ],
   routes: [
     {
       name: "fan-out-order-created",
