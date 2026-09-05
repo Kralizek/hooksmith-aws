7e7e9cd9b45734245d3a3122a081bc23fb1396ec
Renato Golia <renato@golia.tech>
2026-09-05 20:45:22 +0200

    Migrate SNS example logging context

--- examples/lambda-sns/handler.ts
+++ examples/lambda-sns/handler.ts
@@ -2,7 +2,11 @@ import type { Config, Context } from "@hooksmith/core";
 import { fromSns } from "@hooksmith/aws/sns";
 import { createProcessor } from "@hooksmith/aws-lambda";
 import { createHandler } from "@hooksmith/aws-lambda/sns";
-import { createRuntime } from "@hooksmith/runtime";
+import {
+  createConsoleLogWriter,
+  createLoggerFactory,
+  createRuntime,
+} from "@hooksmith/runtime";
 
 const config: Config = {
   routes: [
@@ -21,7 +25,9 @@ const config: Config = {
   ],
 };
 
-const context: Context = { log: console };
+const context: Context = {
+  logger: createLoggerFactory({ write: createConsoleLogWriter() }),
+};
 const processor = createProcessor(createRuntime(config, context));
 
 export const handler = createHandler(fromSns, processor);
