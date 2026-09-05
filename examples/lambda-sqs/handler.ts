bc948c756807e8e5267684c603aef812a17d6de8
Renato Golia <renato@golia.tech>
2026-09-05 20:45:13 +0200

    Migrate SQS example logging context

--- examples/lambda-sqs/handler.ts
+++ examples/lambda-sqs/handler.ts
@@ -2,7 +2,11 @@ import type { Config, Context } from "@hooksmith/core";
 import { fromSqs } from "@hooksmith/aws/sqs";
 import { createProcessor } from "@hooksmith/aws-lambda";
 import { createHandler } from "@hooksmith/aws-lambda/sqs";
-import { createRuntime } from "@hooksmith/runtime";
+import {
+  createConsoleLogWriter,
+  createLoggerFactory,
+  createRuntime,
+} from "@hooksmith/runtime";
 
 const config: Config = {
   routes: [
@@ -21,7 +25,9 @@ const config: Config = {
   ],
 };
 
-const context: Context = { log: console };
+const context: Context = {
+  logger: createLoggerFactory({ write: createConsoleLogWriter() }),
+};
 const processor = createProcessor(createRuntime(config, context));
 
 export const handler = createHandler(fromSqs, processor, context);
